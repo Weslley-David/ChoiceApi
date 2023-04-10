2b65e0984808cb92903e398f068459e5d0c5b0ac
Weslley David <weslleyvwp@hotmail.com>
2023-04-10 15:48:14 -0300

    feat: coletar questão específica - test - adicionando aquivo de teste

---
+++ __tests__/test.ts
@@ -0,0 +1,11 @@
+describe('Teste do exemplo', () => {
+    it('deve retornar verdadeiro', () => {
+        expect(true).toBeTruthy();
+    });
+});
+
+describe('Coletar questão', ()=>{
+    it('retorna uma questão do tipo json', () =>{
+        expect(JSON)
+    })
+} )
